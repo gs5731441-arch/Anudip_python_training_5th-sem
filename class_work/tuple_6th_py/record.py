20b666e75fa9e6a9a913756fb2a15a609a3975f1
Gopal thakur <Gs5731441@gmail.com>
2026-06-06 10:15:01 +0530

    display all student record

---
+++ class_work/tuple_6th_py/record.py
@@ -0,0 +1,15 @@
+#Task 1: To display all student record
+
+print("--- Student's Records -----")
+
+for record in students:
+
+print("Student id: ", record[0])
+
+print("Name: ", record [1])
+
+print("Course: ,record [2])
+
+print("Fee Paid Rs", record [3])
+
+print("-")
